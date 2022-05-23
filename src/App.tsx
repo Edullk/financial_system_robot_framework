85bc2f35a56bb66f83e5d3d593d38980a6e9fe0e
eduardo_oliveira <eduardogomes3007@gmail.com>
2022-05-23 15:26:12 -0300

    secund commit

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
   return (
     <C.Container>
       <C.Header>
-        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        <C.HeaderText>Sistema Financeiro </C.HeaderText>
       </C.Header>
       <C.Body>
         
