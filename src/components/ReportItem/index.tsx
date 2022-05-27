03551f29f9ad26f7a7683d584ec0a6b0f76ec293
Eduardo.oliveira <eduardo.oliveira@elastic.fi>
2022-05-27 14:27:06 -0300

    removed id

--- src/components/ReportItem/index.tsx
+++ src/components/ReportItem/index.tsx
@@ -2,7 +2,7 @@ import * as C from './styles';
 
 export const ReportItem = () => {
     return (
-        <C.Table id="table_area">
+        <C.Table>
             <C.Link  target="_blank" href="http://localhost:3000/log.html" rel="noreferrer">Relatório dos testes</C.Link>
         </C.Table>
     );
